2c7a365bd86a1647a36ad424beba72215ad6ffe9
BOOK-M84J6054QC\airzo <airzosel2@gmail.com>
2023-05-10 02:14:40 +0900

    - 카카오 지도맵 연동 1. Kakao App 추가 및 안드로이드 해시키 입력 2. SDK 추가 (.so 인식불가로 .jar 로 변경하여 강제로 추가) 및 기타 설정 3. 앱키 입력 4. 지도뷰 표시 코드 추가 * 카카오 api 는 가상모바일기기로 시뮬레이션 불가 (https://developer.android.com/studio/run/device?hl=ko 참조)

--- ShowMeTheBeer/app/src/main/java/com/sese/showmethebeer/BeerStoreManagerActivity.java
+++ ShowMeTheBeer/app/src/main/java/com/sese/showmethebeer/BeerStoreManagerActivity.java
@@ -3,12 +3,35 @@ package com.sese.showmethebeer;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.ViewGroup;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import net.daum.mf.map.api.MapView;
 public class BeerStoreManagerActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_beer_store_manager);
+
+//        /* 키 해시 얻기*/
+//        try {
+//            PackageInfo info = getPackageManager().getPackageInfo(getPackageName(), PackageManager.GET_SIGNATURES);
+//            for (Signature signature : info.signatures) {
+//                MessageDigest md = MessageDigest.getInstance("SHA");
+//                md.update(signature.toByteArray());
+//                Log.d("키해시는 :", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+//            }
+//        } catch (PackageManager.NameNotFoundException e) {
+//            e.printStackTrace();
+//        } catch (NoSuchAlgorithmException e) {
+//            e.printStackTrace();
+//        }
+
+        MapView mapView = new MapView(this);
+
+        ViewGroup mapViewContainer = (ViewGroup) findViewById(R.id.map_view);
+        mapViewContainer.addView(mapView);
     }
 }
